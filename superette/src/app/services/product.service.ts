20548e063ad912ca598c92e7444bad233f332ba5
BladeRED <pokeblade43@gmail.com>
2021-12-14 12:14:35 +0100

    fin exo superette pour le moment

--- superette/src/app/services/product.service.ts
+++ superette/src/app/services/product.service.ts
@@ -2,46 +2,37 @@ import { Injectable } from '@angular/core';
 import { Product } from '../models/Product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
   products: Product[];
 
   constructor() {
-
-    this.products =[];
+    this.products = [];
 
     for (let i = 0; i < 15; i++) {
-
       this.products.push(new Product(i, 'Name' + i, true));
-
     }
-   }
-
-  changeDispoById(productId: number): Promise<Product> {
-
-    return new Promise<Product>((resolve, reject) => {
-
-      for(let[index, product] of this.products.entries()) {
-
-        if(product.id === productId) {this.products[index].status = !this.products[index].status};
-        resolve(this.products[index])
-        break;
-      }
-
-    })
-
   }
 
-   UpdateAllDispo(newDispo: boolean): Promise<Product[]> {
+  changeDispoById(productId: number): Promise<Product> {
+    return new Promise<Product>((resolve, reject) => {
+      for (let [index, product] of this.products.entries()) {
+        if (product.id === productId) {
+          this.products[index].status = !this.products[index].status;
+          resolve(this.products[index]);
+          break;
+        }
 
+
+      }
+    });
+  }
+
+  UpdateAllDispo(newDispo: boolean): Promise<Product[]> {
     return new Promise<Product[]>((resolve, reject) => {
-
-      this.products.forEach(product => product.status = newDispo);
+      this.products.forEach((product) => (product.status = newDispo));
       resolve(this.products);
-    })
-
-
-   }
+    });
+  }
 }
