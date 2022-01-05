ea9e1d6d862dc7406d287deb605a9e1b5da9529d
BladeRED <pokeblade43@gmail.com>
2022-01-05 21:03:53 +0100

    login, check

--- series-manager/src/app/services/login/login.service.ts
+++ series-manager/src/app/services/login/login.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
 
-  constructor() { }
+  token!: BehaviorSubject<string>;
+  constructor() {
+
+this.token = new BehaviorSubject<string>('')
+
+
+  }
+
+  signin(user: string, password: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      if (user === 'Administrateur' && password === 'azerty') {
+        this.token.next('TuEsUnSorcierHarryDu42!');
+        resolve()
+      }else{reject('Quel erreur du joueur français!')}
+    });
+  }
+
+  signOut(): Promise<void> {
+
+return new Promise<void>((resolve, reject) => {this.token.next('')
+resolve()})
+
+  }
 }
--- series-manager/src/app/views/login/login.component.ts
+++ series-manager/src/app/views/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user!:string;
+  password!:string;
+  errMsg!:string;
+
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
+
+    this.user = 'Administrateur'
+    this.password = 'azerty'
   }
 
+  onSubmitLogForm(): void {
+    this.errMsg ='';
+
+    this.loginService.signin(this.user, this.password)
+    .then(() => {
+      //redirect to books view
+      this.router.navigateByUrl('/series')
+    })
+    .catch((errMsg) => {//reject})
+      this.errMsg = errMsg;
+  })
+};
 }
+
+
